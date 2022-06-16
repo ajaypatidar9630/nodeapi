a02bd8736b18536d602cf32b370f9e150575517f
ajaypatidar9630 <107549249+ajaypatidar9630@users.noreply.github.com>
2022-06-16 15:37:02 +0530

    Add files via upload
    update done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+const { Server } = require('http');
 
 app.get('/partner/customer', function (req, res) {
    fs.readFile( __dirname + "/" + "customers.json", 'utf8', function (err, data) {
@@ -11,8 +12,9 @@ app.get('/partner/customer', function (req, res) {
 
 
 
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+   console.log('server listen');
+}
+
+)
